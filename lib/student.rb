7d568e23bf44e5bda7100570998df822902c03fe
github username <github email address>
2020-05-04 00:43:19 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -21,9 +21,9 @@ class Student
     end
 
     def self.drop_table
-      sql = <<-SQL
-        DROP TABLE students
-        SQL
+    
+      sql = "DROP TABLE IF EXISTS students"
+    
         DB[:conn].execute(sql)
     end
 
@@ -32,6 +32,7 @@ class Student
           INSERT INTO students (name, grade)
           VALUES (?, ?)
           SQL
+
           DB[:conn].execute(sql, self.name, self.grade)
           @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
